4af588516bc2472be53f164ac2ed900ac848cd88
PeaceJohn20 <159129872+PeaceJohn20@users.noreply.github.com>
2024-07-30 12:07:29 +0100

    peace commit

--- Swing/App.java
+++ Swing/App.java
@@ -2,10 +2,10 @@ package Swing;
 
 public class App {
     // peacejohn20
-    // public static void main(String[] args) {
-    //     PasswordGeneratorGUI pgGUI = new PasswordGeneratorGUI();
-    //     pgGUI.setVisible(true);
+    public static void main(String[] args) {
+        PasswordGeneratorGUI pgGUI = new PasswordGeneratorGUI();
+        pgGUI.setVisible(true);
         
-    // }
+    }
     
 }
